c533786b8880dc95a4c773d1ca2ef62bc29b8747
Andrei-WongE <awonge01@student.bbk.ac.uk>
2025-07-06 16:00:32 +0100

    Additional options and enhanced colours

--- data_clean.R
+++ data_clean.R
@@ -1,1 +1,34 @@
 # See cities_db project shiny_data.R for the data cleaning process
+
+# NEXT STEPS
+# Select variables according simple sum, areal weigthed sum, population weigted sum or other
+# Write code and calculate new variable according urban definition
+# Create database for each urban definition
+# Create key table to match urban definitions and metadata
+
+# First the list of deliverables I expect in the folder (clearly arranged in the folder)
+#   Now the list of graphs:
+#   
+#   First of all, I expect to have  ability to select one main city, and 8 comparators: 4 direct comparators, and 4 aspirational. Both groups should be identified by color – E.g. green for aspirational, blue for direct.
+# 
+# Comparison of economic growth – GVA OE 15 years line chart till 2021.
+# Comparison of employment growth: OE 15 years line chart till 2021.
+# Comparison of growth of Night lights: total of eFUAs (15 years) – line chart
+# Comparison of growth of total build up area eFUA (25 years) – line chart
+# Comparison of gdp growth using GDP from UCDB – sum of all centers, (not sure what the time range is for it  - so make a call)
+# Comparison of the structure of GVA : OE, % bar charts. 2019
+# Comparison of structure of employment: OE, % bar charts 2019
+# Timeseries bar charts for shifts of employment structure for each city (hopefully you can stack them on one page). OE – 15 years.til 2021
+# Timeseries bar charts for shifts of GVA structure for each city. Till 2021
+# Comparison structure bar charts for high – low skill employment using the data for eFUAs from bens dataset, use lates year available.
+# GHG emissions per capita over time comparison  - line chart. (for as long as available) – UCBD
+# Share of population living  in areas in areas exposed to 10 year floods.  – USDB, a simple bar chart.
+# 
+# Bonus
+# 
+# Comparison of gdp growth using GDP from UCDB – oonly for the main urban center. , line chart,
+# Share of population exposed to 20 year coastal floods  - bar chart, UDSB
+# Share of green in build up areas – bar chart.UCDB
+# Increase of annual mean temperatures – bar chart UCDB
+# Road network density – bar chart  - UCDB
+# CISI index for all sectors.  – UCDB – population average for all urban centers.
